4f944b9c741508f9723d9636777fb1073dd8ae3c
Goran <goranvukojicic@gmail.com>
2022-11-17 14:35:49 +0100

    add repository with basic crud operations

---
+++ Project_Tasks.Data/IProjectTasksRepository.cs
@@ -0,0 +1,10 @@
+﻿using Project_Tasks.Data.Entities;
+
+namespace Project_Tasks.Data
+{
+    public interface IProjectTasksRepository
+    {
+        void AddProject(Project project);
+        IEnumerable<Project> GetAllProjects();
+    }
+}
---
+++ Project_Tasks.Data/ProjectTasksRepository.cs
@@ -0,0 +1,29 @@
+﻿using Project_Tasks.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Tasks.Data
+{
+    public class ProjectTasksRepository : I
+    {
+        private readonly ProjectTasksDbContext dbContext;
+
+        public ProjectTasksRepository(ProjectTasksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void AddProject(Project project)
+        {
+            dbContext.Projects.Add(project);
+            dbContext.SaveChanges();
+        }
+        public IEnumerable<Project> GetAllProjects()
+        {
+            return dbContext.Projects.ToList();
+        }
+    }
+}
